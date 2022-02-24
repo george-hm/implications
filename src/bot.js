fdd246bebd0c979a9d9e7bdd397b3c9333c03ab7
George <georgehm@pm.me>
2022-02-24 08:13:25 +0000

    Remove message listener

--- src/bot.js
+++ src/bot.js
@@ -4,41 +4,6 @@ const User = require('./model/discord/User.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.on('messageCreate', async message => {
-    const { member } = message;
-    // only care about messages sent in guilds
-    if (!member || member.user.bot) {
-        return;
-    }
-
-    const user = new User(message.member.user);
-    // TODO: build triggers properly later
-    const { channelId } = message;
-    if (channelId !== '723977119746490379' && channelId !== '451772950404792342') {
-        return;
-    }
-
-    const checks = [
-        /(^|\s)(lfg|lf|looking for (someone|group))(\s|$)/i,
-        /(up|down) for (hell |)gates/i,
-        /(looking for|need) (2v2|1v1|2x2|1x1)/i,
-    ];
-    let checkMatched = false;
-    for (const check of checks) {
-        if (check.test(message.content)) {
-            checkMatched = true;
-            break;
-        }
-    }
-
-    if (!checkMatched) {
-        return;
-    }
-
-    const replyMessage = `${user.getMention()} to get access to **Looking for Group** channel, go to <#893560390385041408>`;
-    message.reply(replyMessage);
-});
-
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand() && !interaction.isButton() && !interaction.isSelectMenu()) return;
 
