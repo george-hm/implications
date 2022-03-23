b8772bad53ca15f679af89fb9c3ef826ed43ebf7
George <georgehm@pm.me>
2022-03-23 08:48:12 +0000

    Model for cards/dealing/decks

---
+++ src/model/Card.js
@@ -0,0 +1,60 @@
+const SUITES = ['clubs', 'diamonds', 'hearts', 'spades'];
+const RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+class Card {
+    constructor(suite, rank) {
+        this.suite = suite;
+        this.rank = rank;
+        this.value = Number.parseInt(rank) || 10;
+    }
+
+    getSuiteArt() {
+        switch (this.suite) {
+            case 'clubs':
+                return '♣';
+            case 'diamonds':
+                return '♦';
+            case 'hearts':
+                return '♥';
+            case 'spades':
+                return '♠';
+            default:
+                return '?';
+        }
+    }
+
+    getSummary() {
+        return ` -\n|${this.rank}|\n|${this.getSuiteArt()}|\n-`;
+    }
+
+    static emptySummary() {
+        return ' -\n|?|\n|?|\n-';
+    }
+
+    static getRandomDeck() {
+        const cards = [];
+        SUITES.forEach(suite => {
+            RANKS.forEach(rank => {
+                cards.push(new Card(suite, rank));
+            });
+        });
+        const shuffledDeck = [];
+        while (cards.length > 0) {
+            const index = Math.floor(Math.random() * cards.length);
+            shuffledDeck.push(cards[index]);
+            cards.splice(index, 1);
+        }
+        return cards;
+    }
+
+    static getTotalValue(cards) {
+        const total = cards.reduce((acc, card) => acc + card.value, 0);
+        const includesAce = cards.some(card => card.rank === 'A');
+        if (includesAce && total > 21) {
+            return total - 10;
+        }
+        return total;
+    }
+}
+
+module.exports = Card;
