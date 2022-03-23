22be42aa1175612225ca308cc83425cacdc54916
George <georgehm@pm.me>
2022-03-23 08:47:38 +0000

    Rewrite register for easier updating + new version

--- src/registerCommands.js
+++ src/registerCommands.js
@@ -1,34 +1,23 @@
 /* eslint-disable no-await-in-loop */
-const axios = require('axios');
+require('dotenv').config({ path: '../process.env' });
+const { Routes } = require('discord-api-types/v9');
+const { REST } = require('@discordjs/rest');
 const commandList = require('./command/index.js').mapping;
 
-const APPLICATION_ID = 'REPLACE_ME';
-const BOT_TOKEN = 'REPLACE_ME';
+const { BOT_TOKEN, APPLICATION_ID } = process.env;
 // leave this null if you want to update commands globally
-const GUILD_ID = null;
+const GUILD_ID = process.argv[2] ? process.env.GUILD_ID : null;
+
+console.log(`Updating commands for ${GUILD_ID ? 'guild' : 'global'} ${GUILD_ID || ''}`);
 
 const commands = Object.values(commandList).map(command => command.toJSON());
 
-async function main() {
-    for (let i = 0; i < commands.length; i++) {
-        const command = commands[i];
-        console.log(command.name);
-        const url = `https://discord.com/api/v8/applications/${APPLICATION_ID}${GUILD_ID ? `/guilds/${GUILD_ID}` : ''}/commands`;
-        try {
-            await axios({
-                url,
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bot ${BOT_TOKEN}`,
-                },
-                data: JSON.stringify(command),
-            });
-        } catch (err) {
-            console.log(err.response.status);
-        }
-    }
-    console.log('Registered all commands');
-}
+const rest = new REST({ version: '9' }).setToken(BOT_TOKEN);
 
-main();
+const routeToUse = GUILD_ID ?
+    Routes.applicationGuildCommands(APPLICATION_ID, GUILD_ID) :
+    Routes.applicationCommands(APPLICATION_ID);
+
+rest.put(routeToUse, { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
